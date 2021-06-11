b0c4da290787b804b61c014b896ee079997fed9a
hbobenicio <hbobenicio@gmail.com>
2021-06-10 23:33:00 -0300

    refactoring test from separate dir into same folder as unit source

--- src/hbo/arraylist.test.cpp
+++ src/hbo/arraylist.test.cpp
@@ -1,7 +1,7 @@
+#include <hbo/arraylist.h>
+
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include <catch2/catch.hpp>
-
-#include <hbo/arraylist.h>
 
 TEST_CASE("Basic Tests", "[arraylist]") {
     hbo_arraylist list = arraylist_from_capacity(3);
